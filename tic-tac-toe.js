081c76013e496b9d826f97780e0763a7bcf96128
Kayla Blake <kayla.blake16@gmail.com>
2020-10-09 14:55:27 -0500

    Exercise 2,5 & 6

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,26 +2,87 @@
 
 window.onload = function()
 {
+    let player = 1;
     var board = document.getElementById("board");
     var squares = board.querySelectorAll("div");
-    var i;
-    for (i=0;i < squares.length;i++)
+    var status = document.getElementById("status");
+    var tiles = [];
+
+    for (var i= 0;i < squares.length; i++)
     {
         squares[i].classList.add("square");
+        click(squares[i]);
     }
+
+    
+    function click(item)
+    {
+        item.onclick = function(element)
+        {
+            console.log("Clicked Square ");
+
+            if (player == 1)
+            {
+                element.target.innerHTML = "X";
+                element.target.classList.remove("O");
+                element.target.className += " X";
+                console.log("Play X");
+                element.target.onclick = null;
+                player = 2;
+            }
+            else
+            {
+                element.target.innerHTML = "O";
+                element.target.classList.remove("X");
+                element.target.className += " O";
+                console.log("Play O");
+                element.target.onclick = null;
+                player = 1;
+            }
+
+        }
+
+    }
+
+    /*function hover(item)
+    {
+        var text = element.target.innerHTML
+        if (text === "O")
+        {
+            item.onmouseover = function(element)
+            {
+                console.log("Mouse Over Square");
+                element.target.classList.add("hover.O");
+            }
+
+        }
+        else
+        {
+            item.onmouseover = function(element)
+            {
+                console.log("Mouse Over Square");
+                element.target.classList.add("hover");
+            }
+
+        }
+    }*/
+
 
     var button = document.querySelector("button");
 
     button.onmouseover = function(element)
     {
-        console.log("Mouse Over");
+        console.log("Mouse Over Button");
         button.classList.add("btn:hover");
     }
     button.onclick = function(element)
     {
-        for (i=0;i < squares.length;i++)
+        console.log("Clicked Button");
+        for (var i=0;i < squares.length;i++)
         {
-        squares[i].classList.add("square");
+            squares[i].innerHTML = "";
+            click(squares[i]);
         }
+        console.log("Clear Board");
     }
 }
